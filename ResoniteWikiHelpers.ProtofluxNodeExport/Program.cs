e615747996ea0d8925a1ae8f3050550de0064f92
Arne Kiesewetter <banane9@live.de>
2024-01-06 02:30:46 +0100

    Moaaaar nodes and colors

--- ResoniteWikiHelpers.ProtofluxNodeExport/Program.cs
+++ ResoniteWikiHelpers.ProtofluxNodeExport/Program.cs
@@ -3,6 +3,7 @@ using FrooxEngine;
 using ProtoFlux.Core;
 using ProtoFlux.Runtimes.Execution.Nodes.Actions;
 using ProtoFlux.Runtimes.Execution.Nodes.Operators;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
@@ -11,119 +12,162 @@ namespace ResoniteWikiHelpers.ProtofluxNodeExport
 {
     internal class Program
     {
+        private static readonly string _allNodesNotice =
+@"<!-----------------------------------------------------------------------+
+ ! This file has been generated using a script. Do not edit it manually. !
+ +----------------------------------------------------------------------->
+
+[All Nodes Header](./README_prefix.md ':include')
+";
+
+        private static readonly string _includeTemplate = "[Category {0}](./{1}/README.md ':include')";
         private static readonly string[] _nodeAssemblies = new[] { "FrooxEngine", "ProtoFlux.Core", "ProtoFlux.Nodes.Core", "ProtoFlux.Nodes.FrooxEngine" };
 
         private static readonly Type _nodeType = typeof(INode);
 
         private static readonly string _notice =
-@"<!---------------------------------------------------------------------!
+@"<!-----------------------------------------------------------------------+
  ! This file has been generated using a script. Do not edit it manually. !
- ! Edit the protoflux.css file instead.                                  !
- !-----------------------------------------------------------------------!
+ ! Edit the individual node pages instead.                               !
+ +----------------------------------------------------------------------->
+
+## {0}
+
 ";
 
         private static readonly Regex _rtfRegex = new(@"</?.*?/?>");
 
         private static readonly string _tableFooterTemplate =
-@"| {0} |  |  |
+@"| {0} | {1} |  |
 <!-- ProtofluxNode:end -->
 
 ";
 
         private static readonly string _tableHeaderTemplate =
-@"<!-- panels:start -->
-<!-- div:title-panel -->
-### {0}
-<!-- div:left-panel -->
+@"### {0}
 
-<!-- div:right-panel -->
 <!-- ProtofluxNode:start -->
 | {0} | Type | Label |
 | --- | ---- | ----- |";
 
         private static readonly string _tableRowTemplate = "| {0} | {1} | {2} |";
 
-        private static string FormatRow(bool input, string type, string name)
-            => string.Format(_tableRowTemplate, input ? "input" : "output", type, name.Replace("|", "\\|"))
+        private static string FormatRow(string element, string type, string name)
+            => string.Format(_tableRowTemplate, element, type.Replace('`', '_'), name.Replace("|", "\\|"))
                 .Replace("`", "\\`");
 
-        private static string GetAsyncOp(bool isAsync)
-            => isAsync ? "AsyncOperation" : "SyncOperation";
+        private static string GetOperationName(bool isSync, bool isAsync)
+            => isSync ?
+                (isAsync ? "MixedOperation" : "SyncOperation")
+                : (isAsync ? "AsyncOperation" : "NoneOperation");
 
         private static void Main(string[] args)
         {
-            using var outputFile = new FileStream("ProtofluxNodes.md", FileMode.OpenOrCreate);
-            outputFile.SetLength(0);
+            if (Directory.Exists("export"))
+                Directory.Delete("export", true);
 
-            using var writer = new StreamWriter(outputFile);
+            Directory.CreateDirectory("export");
 
-            writer.Write(_notice);
+            using var allNodesFile = new FileStream(Path.Combine("export", "README.md"), FileMode.OpenOrCreate);
+            using var allNodesWriter = new StreamWriter(allNodesFile);
 
-            var nodes = _nodeAssemblies.Select(name => Assembly.LoadFrom($"./{name}.dll"))
+            allNodesWriter.WriteLine(_allNodesNotice);
+
+            var nodeCategories = _nodeAssemblies.Select(name => Assembly.LoadFrom($"./{name}.dll"))
                 .SelectMany(assembly => assembly.GetTypes())
                 .Where(type => !type.IsAbstract && type.IsPublic && type.IsAssignableTo(_nodeType))
-                .Select(nodeType => (Type: nodeType, Metadata: NodeMetadataHelper.GetMetadata(nodeType), Path: nodeType.GetCustomAttributes<NodeCategoryAttribute>(false).FirstOrDefault()?.Name))
-                .Where(node => node.Path is not null && node.Metadata.Name is not null)
-                .DistinctBy(node => string.IsNullOrWhiteSpace(node.Metadata.Overload) ? node.Type.FullName : node.Metadata.Overload)
-                .OrderBy(node => node.Metadata.Name);
-
-            foreach (var node in nodes)
-            {
-                writer.WriteLine(string.Format(_tableHeaderTemplate, _rtfRegex.Replace(node.Metadata.Name.Replace("|", "\\|"), "")));
-
-                var inputElements = node.Metadata.FixedOperations
-                    .Concat<IElementMetadata>(node.Metadata.DynamicOperations)
-                    .Concat(node.Metadata.FixedInputs)
-                    .Concat(node.Metadata.DynamicInputs);
-
-                var outputElements = node.Metadata.FixedImpulses
-                    .Concat<IElementMetadata>(node.Metadata.DynamicImpulses)
-                    .Concat(node.Metadata.FixedOutputs)
-                    .Concat(node.Metadata.DynamicOutputs);
-
-                foreach (var element in inputElements.Interleave(outputElements))
+                .Select(nodeType =>
                 {
-                    switch (element)
+                    var metadata = NodeMetadataHelper.GetMetadata(nodeType);
+                    var path = NullConcat("Root/", nodeType.GetCustomAttributes<NodeCategoryAttribute>(true).FirstOrDefault()?.Name) ?? "Root";
+                    var name = StringHelper.BeautifyName(metadata.Name ?? Path.GetExtension(metadata.Overload) ?? nodeType.Name);
+
+                    return (Type: nodeType, Metadata: metadata, Path: path, Name: name);
+                })
+                .Where(node => node.Path is not null)
+                .GroupBy(node => node.Metadata.Overload ?? node.Name)
+                .Select(nodeGroup => nodeGroup.First()) // Select by something else?
+                .GroupBy(node => node.Path)
+                .OrderBy(nodeCategory => nodeCategory.Key);
+
+            foreach (var nodeCategory in nodeCategories)
+            {
+                var path = Path.Combine("export", nodeCategory.Key);
+                Directory.CreateDirectory(path);
+
+                allNodesWriter.WriteLine(_includeTemplate, nodeCategory.Key, nodeCategory.Key.Replace(" ", "%20"));
+                allNodesWriter.WriteLine();
+
+                using var outputFile = new FileStream(Path.Combine(path, "README.md"), FileMode.OpenOrCreate);
+                using var writer = new StreamWriter(outputFile);
+
+                writer.Write(_notice, nodeCategory.Key);
+
+                foreach (var node in nodeCategory.OrderBy(node => node.Metadata.Overload ?? node.Name))
+                {
+                    writer.WriteLine(string.Format(_tableHeaderTemplate, _rtfRegex.Replace(node.Name.Replace("`", "\\`").Replace("|", "\\|"), "")));
+
+                    var inputElements = node.Metadata.FixedOperations
+                        .Concat<IElementMetadata>(node.Metadata.DynamicOperations)
+                        .Concat(node.Metadata.FixedInputs)
+                        .Concat(node.Metadata.DynamicInputs);
+
+                    var outputElements = node.Metadata.FixedImpulses
+                        .Concat<IElementMetadata>(node.Metadata.DynamicImpulses)
+                        .Concat(node.Metadata.FixedOutputs)
+                        .Concat(node.Metadata.DynamicOutputs);
+
+                    foreach (var element in inputElements.Interleave(outputElements))
                     {
-                        case OperationMetadata operation:
-                            writer.WriteLine(FormatRow(true, GetAsyncOp(operation.IsAsync), operation.Name));
-                            break;
+                        switch (element)
+                        {
+                            case OperationMetadata operation:
+                                writer.WriteLine(FormatRow("input", GetOperationName(!operation.IsAsync, operation.IsAsync), operation.Name));
+                                break;
 
-                        case OperationListMetadata operationList:
-                            writer.WriteLine(FormatRow(true, GetAsyncOp(operationList.SupportsAsync), operationList.Name));
-                            break;
+                            case OperationListMetadata operationList:
+                                writer.WriteLine(FormatRow("inputlist", GetOperationName(operationList.SupportsSync, operationList.SupportsAsync), operationList.Name));
+                                break;
 
-                        case InputMetadataBase input:
-                            writer.WriteLine(FormatRow(true, input.Field.FieldType.IsConstructedGenericType ? input.Field.FieldType.GenericTypeArguments[0].Name : input.Field.FieldType.Name, input.Name));
-                            break;
+                            case InputMetadata input:
+                                writer.WriteLine(FormatRow("input", input.Field.FieldType.IsConstructedGenericType ? input.Field.FieldType.GenericTypeArguments[0].Name : input.Field.FieldType.Name, input.Name));
+                                break;
 
-                        case ImpulseMetadata impulse:
-                            writer.WriteLine(FormatRow(false, impulse.Type.ToString(), impulse.Name));
-                            break;
+                            case InputListMetadata input:
+                                writer.WriteLine(FormatRow("inputlist", input.Field.FieldType.IsConstructedGenericType ? input.Field.FieldType.GenericTypeArguments[0].Name : input.Field.FieldType.Name, input.Name));
+                                break;
 
-                        case ImpulseListMetadata impulseList:
-                            writer.WriteLine(FormatRow(false, impulseList.Type?.ToString() ?? "null", impulseList.Name));
-                            break;
+                            case ImpulseMetadata impulse:
+                                writer.WriteLine(FormatRow("output", impulse.Type.ToString(), impulse.Name));
+                                break;
 
-                        case OutputMetadata output:
-                            writer.WriteLine(FormatRow(false, output.OutputType.Name, output.Name));
-                            break;
+                            case ImpulseListMetadata impulseList:
+                                writer.WriteLine(FormatRow("outputlist", impulseList.Type?.ToString() ?? "null", impulseList.Name));
+                                break;
 
-                        case OutputListMetadata outputList:
-                            writer.WriteLine(FormatRow(false, outputList.TypeConstraint?.Name ?? "*", outputList.Name));
-                            break;
+                            case OutputMetadata output:
+                                writer.WriteLine(FormatRow("output", output.OutputType.Name, output.Name));
+                                break;
 
-                        default:
-                            Console.WriteLine(element);
-                            break;
+                            case OutputListMetadata outputList:
+                                writer.WriteLine(FormatRow("outputlist", outputList.TypeConstraint?.Name ?? "*", outputList.Name));
+                                break;
+
+                            default:
+                                Console.WriteLine(element);
+                                break;
+                        }
                     }
+
+                    writer.WriteLine(string.Format(_tableFooterTemplate, node.Path, node.Type.FullName!.Replace("`", "\\`")));
                 }
 
-                writer.WriteLine(string.Format(_tableFooterTemplate, Path.GetFileName(node.Path) ?? ""));
+                writer.Flush();
+                writer.Close();
             }
-
-            writer.Flush();
-            writer.Close();
         }
+
+        private static string? NullConcat(string? a, string? b)
+                    => a is null || b is null ? null : a + b;
     }
 }
--- ResoniteWikiHelpers.TypeColors/Program.cs
+++ ResoniteWikiHelpers.TypeColors/Program.cs
@@ -1,7 +1,13 @@
 ﻿using Elements.Core;
+using Elements.Quantity;
 using FrooxEngine;
 using FrooxEngine.ProtoFlux;
 using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+using ProtoFlux.Runtimes.Execution.Nodes.FrooxEngine.Security;
+using ProtoFlux.Runtimes.Execution.Nodes.Math;
+using System.Globalization;
+using TwitchLib.Client.Enums;
 
 namespace ResoniteWikiHelpers.TypeColors
 {
@@ -11,7 +17,7 @@ namespace ResoniteWikiHelpers.TypeColors
 @".PFN-{0} {{
   stroke: rgb({1});
   fill: rgb({1}, 0.174);
-  background-image: linear-gradient(rgba({1}, 0.3), rgba({1}, 0.3));
+  background-image: linear-gradient(rgba({1}, 0.7), rgba({1}, 0.7));
 }}
 
 ";
@@ -35,7 +41,54 @@ namespace ResoniteWikiHelpers.TypeColors
 
         private static void Main(string[] args)
         {
-            var types = GenericTypesAttribute.GetTypes(GenericTypesAttribute.Group.EnginePrimitivesAndEnums);
+            var types = GenericTypesAttribute.GetTypes(GenericTypesAttribute.Group.EnginePrimitivesAndEnums)
+                .Concat(new[]
+                {
+                    typeof(object),
+                    typeof(Nullable), typeof(Nullable<>),
+                    typeof(Guid), typeof(RefID),
+                    typeof(IValue), typeof(IValue<>), typeof(IVariable<,>),
+                    typeof(IField), typeof(IField<>),
+                    typeof(ISyncRef), typeof(ISyncRef<>),
+                    typeof(TangentPointFloat),
+                    typeof(bobool3ol), typeof(half),
+                    typeof(ColorProfile),
+                    typeof(CultureInfo), typeof(IFormatProvider),
+                    typeof(DateTimeKind), typeof(DayOfWeek),
+                    typeof(StringComparison),
+                    typeof(User), typeof(UserRef),
+                    typeof(UserRoot), typeof(BodyNode),
+                    typeof(FingerType), typeof(FingerSegmentType), typeof(IFingerPoseSource),
+                    typeof(CharacterController), typeof(IAvatarAnchor),
+                    typeof(JoinRequestHandle), typeof(Platform),
+                    typeof(IWorldElement), typeof(Slot), typeof(Component), typeof(IComponent),
+                    typeof(ProtoFluxNode),
+                    typeof(ICollider), typeof(BoundingBox),
+                    typeof(Rect), typeof(BoundingBox2D),
+                    typeof(ReflectionProbe),
+                    typeof(ComponentHandling),
+                    typeof(IFocusable),
+                    typeof(UsersAssetLoadProgress<>), typeof(AssetLoadState),
+                    typeof(IPlayable), typeof(AudioOutput), typeof(SyncPlayback),
+                    typeof(AudioRolloffMode), typeof(AudioDistanceSpace), typeof(AudioDistanceSpace), typeof(AudioTypeGroup),
+                    typeof(RawDataTool), typeof(ITool),
+                    typeof(IWorldLink), typeof(Userspace.WorldRelation),
+                    typeof(LocaleResource),
+                    typeof(IAssetProvider), typeof(IAssetProvider<>),
+                    typeof(Camera), typeof(Elements.Assets.TextureFormat),
+                    typeof(TextureWrapMode), typeof(Elements.Assets.WrapMode),
+                    typeof(StaticAudioClip), typeof(StaticMesh),
+                    typeof(SpriteProvider),typeof(Texture2D), typeof(StaticTexture2D),
+                    typeof(Texture3D),typeof(StaticTexture3D),
+                    typeof(Grabber), typeof(IGrabbable),
+                    typeof(ILocomotionModule),
+                    typeof(Animation),
+                    typeof(ITouchable), typeof(EventState), typeof(TouchType), typeof(TouchEventType),
+                    typeof(CurvePreset),
+                    typeof(BadgeColor), typeof(SubscriptionPlan),
+                    typeof(WebsocketClient),
+                    });
+
             using var output = new FileStream("protofluxTypes.css", FileMode.OpenOrCreate);
             output.SetLength(0);
 
@@ -46,17 +99,35 @@ namespace ResoniteWikiHelpers.TypeColors
             foreach (var impulseType in Enum.GetValues<ImpulseType>())
             {
                 var color = impulseType.GetImpulseColor();
-                writer.Write(string.Format(_cssTemplate, impulseType.ToString(), ColorXToString(color)));
+                writer.Write(_cssTemplate, impulseType.ToString(), ColorXToString(color));
             }
 
-            writer.Write(string.Format(_cssTemplate, "SyncOperation", ColorXToString(DatatypeColorHelper.GetOperationColor(false))));
-            writer.Write(string.Format(_cssTemplate, "AsyncOperation", ColorXToString(DatatypeColorHelper.GetOperationColor(true))));
+            var syncOpColor = DatatypeColorHelper.GetOperationColor(false);
+            var asyncOpColor = DatatypeColorHelper.GetOperationColor(true);
+
+            writer.Write(_cssTemplate, "SyncOperation", ColorXToString(syncOpColor));
+            writer.Write(_cssTemplate, "AsyncOperation", ColorXToString(asyncOpColor));
+            writer.Write(_cssTemplate, "MixedOperation", ColorXToString(MathX.Lerp(syncOpColor, asyncOpColor, .5f)));
 
             foreach (var type in types)
             {
                 var color = type.GetTypeColor();
-                writer.Write(string.Format(_cssTemplate, type.Name, ColorXToString(color)));
+                writer.Write(_cssTemplate, type.Name.Replace('`', '_'), ColorXToString(color));
             }
+
+            var dummyColor = typeof(dummy).GetTypeColor();
+            writer.Write(_cssTemplate, "T", ColorXToString(dummyColor));
+            writer.Write(_cssTemplate, "I", ColorXToString(dummyColor));
+            writer.Write(_cssTemplate, "O", ColorXToString(dummyColor));
+
+            var enumColor = typeof(Enum).GetTypeColor();
+            writer.Write(_cssTemplate, "E", ColorXToString(enumColor));
+
+            var unitColor = typeof(IQuantity<>).GetTypeColor();
+            writer.Write(_cssTemplate, "U", ColorXToString(unitColor));
+
+            var assetColor = typeof(IAsset).GetTypeColor();
+            writer.Write(_cssTemplate, "A", ColorXToString(unitColor));
 
             writer.Flush();
             writer.Close();
